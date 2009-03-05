5e36833c1e1b812837ef42b629981808fb07e0cb
faramir <faramir@users.noreply.github.com>
2009-03-05 22:52:07 +0000

    poprawiona wersja bazy, usuniete teksty komentujace z dokumentacji. wygenerowany plik pdf z dokumentacja

--- res/baza.sql
+++ res/baza.sql
@@ -1,94 +1,111 @@
 /* tabele */
 
-CREATE TABLE USERS (    id                  serial         not null    primary key,
-                        firstName           text,
-                        lastName            text,
-                        eMail               text,
-                        birthDate           date,
-                        login               varchar(20),
-                        pass                varchar(10),
-                        address             text,
-                        school              text,
-                        tutor               text,
-                        gaduGadu            int,
-                        eMailNotification   int
-                    );
+CREATE TABLE USERS (
+        id                  serial primary key,
+        firstName           varchar(40),
+        lastName            varchar(40),
+        eMail               varchar(40),
+        birthDate           date,
+        login               varchar(20),
+        pass                varchar(32),
+        address             varchar(80),
+        school              varchar(80),
+        tutor               text,
+        eMailNotification   int
+);
 
-CREATE TABLE ROLES (    id                  serial         not null    primary key,
-                        name                text,
-                        addContest          int,
-                        editContest         int,
-                        delContest          int,
-                        addSeries           int,
-                        editSeries          int,
-                        delSeries           int,
-                        contestant          int
-                    );
+CREATE TABLE ROLES (  
+        id                  serial primary key,
+        name                varchar(40),
+        addContest          boolean,
+        editContest         boolean,
+        delContest          boolean,
+        addSeries           boolean,
+        editSeries          boolean,
+        delSeries           boolean,
+        addProblem          boolean,
+        editProblem         boolean,
+        delProblem          boolean,
+        canRate             boolean,
+        contestant          boolean
+);
 
-CREATE TABLE SERIES (	id		serial	not null	primary key,
-			name		text,
-			startDate	date,
-			endDate		date,
-			freezeDate	date,
-			penaltyTime	int
-			);
+CREATE TABLE SERIES (  
+        id                  serial primary key,
+        name                varchar(40),
+        startDate           date,
+        endDate             date,
+        freezeDate          date,
+        unfreezeDate        date,
+        penaltyTime         int
+);
 
-CREATE TABLE CONTESTS (	id		serial	not null	primary key,
-			name		text,
-			type		int,
-			startDate	date,
-			about		text,
-			rules		text,
-			tech		text,
-			visibility	int
-			);
+CREATE TABLE CONTESTS (
+        id                  serial primary key,
+        name                varchar(120),
+        type                int,
+        startDate           date,
+        about               text,
+        rules               text,
+        tech                text,
+        visibility          int
+);
 
-CREATE TABLE SUBMITS (	id		serial	not null	primary key,
-			sDate		date,
-			result		int,
-			code		text,
-			filename	text
-			);
+CREATE TABLE SUBMITS ( 
+        id                  serial primary key,
+        sDate               date,
+        result              int,
+        code                bytea,
+        filename            varchar(255),
+        notes               text
+);
 
-CREATE TABLE TASKS (	id	serial	not null	primary key,
-			name	text,
-			text	text,
-			pdf	varchar(70),
-			abbrev	varchar(10),
-			memlimit int
-			);
+CREATE TABLE TASKS ( 
+        id                  serial primary key,
+        name                varchar(80),
+        text                text,
+        pdf                 bytea,
+        abbrev              varchar(5),
+        memlimit            int
+);
 
-CREATE TABLE QUESTIONS (	id		serial	not null	primary key,
-				question	text,
-				answer		text,
-				visibility	int
-				);
+CREATE TABLE QUESTIONS ( 
+        id                  serial primary key,
+        question            text,
+        answer              text,
+        visibility          int
+);
 
-CREATE TABLE RESULTS (	id	serial	not null	primary key,
-			points	int,
-			runTime	int,
-			memory	int,
-			notes	text
-			);
+CREATE TABLE RESULTS (
+        id                  serial primary key,
+        points              int,
+        runTime             int,
+        memory              int,
+        notes               text
+);
 
-CREATE TABLE TESTS (	id		serial	not null	primary key,
-			input		text,
-			output		text,
-			timeLimit	int,
-			maxPoints	int,
-			visibility	int
-			);
+CREATE TABLE TESTS (   
+        id                  serial primary key,
+        input               text,
+        output              text,
+        timeLimit           int,
+        maxPoints           int,
+        visibility          int
+);
 
-CREATE TABLE LANGUAGES (	id		serial	not null	primary key,
-				name		text,
-				extension	varchar(7)
-				);
+CREATE TABLE LANGUAGES ( 
+        id                  serial primary key,
+        name                varchar(40),
+        extension           varchar(8)
+);
 
-CREATE TABLE CLASSES (		id		serial	not null	primary key,
-				filename	text,
-				version		int,
-				description	text
-				);
+CREATE TABLE CLASSES ( 
+        id                  serial primary key,
+        filename            varchar(255),
+        version             int,
+        description         varchar(255),
+        code                bytea
+);
 
 /* Powiazadania jeden do wielu */
 ALTER TABLE SERIES ADD contestsId int REFERENCES CONTESTS(id);
@@ -104,27 +121,38 @@ ALTER TABLE TASKS ADD classesId int REFERENCES CLASSES(id);
 
 /* Powiazania wiele do wielu */
 CREATE TABLE TASKS_QUESTIONS (
-				id		serial not null primary key,
-				taskId		int REFERENCES TASKS(id),
-				questionId	int REFERENCES QUESTIONS(id)
-				);
-CREATE TABLE USERS_ROLES ( 	id		serial not null primary key,
-				userId 		int REFERENCES USERS(id),
-			   	roleId		int REFERENCES ROLES(id)
-				);
+        id                  serial primary key,
+        taskId              int REFERENCES TASKS(id),
+        questionId          int REFERENCES QUESTIONS(id)
+);
+CREATE TABLE USERS_ROLES (  
+        id                  serial primary key,
+        userId              int REFERENCES USERS(id),
+        roleId              int REFERENCES ROLES(id)
+);
 
-CREATE TABLE SERIES_ROLES ( 	id		serial not null primary key,
-				seriesId	int REFERENCES SERIES(id),
-			   	roleId		int REFERENCES ROLES(id)
-				);
+CREATE TABLE SERIES_ROLES ( 
+        id                  serial primary key,
+        seriesId            int REFERENCES SERIES(id),
+        roleId              int REFERENCES ROLES(id)
+);
 
-CREATE TABLE CONTESTS_ROLES ( 	id		serial not null primary key,
-				contestsId 	int REFERENCES CONTESTS(id),
-			   	roleId		int REFERENCES ROLES(id)
-				);
+CREATE TABLE CONTESTS_ROLES ( 
+        id                  serial primary key,
+        contestsId          int REFERENCES CONTESTS(id),
+        roleId              int REFERENCES ROLES(id)
+);
 
-CREATE TABLE LANGUAGES_TASKS ( 	id		serial not null primary key,
-				tasksId 	int REFERENCES TASKS(id),
-			   	languagesId	int REFERENCES LANGUAGES(id)
-				);
-				
+CREATE TABLE LANGUAGES_TASKS (
+        id                  serial primary key,
+        tasksId             int REFERENCES TASKS(id),
+        languagesId         int REFERENCES LANGUAGES(id)
+);
+
+CREATE TABLE SUBMITS_RESULTS_TESTS (  
+        id                  serial primary key,
+        submitsId           int REFERENCES SUBMITS(id),
+        resultsId           int REFERENCES RESULTS(id),
+        testsId             int REFERENCES TESTS(id)
+);
+
