734a2d61ca5e1721c74287c764dfe4fd71dd4dde
dalton <dalton@7685f17d-9d9b-4d2b-a256-61293067a286>
2009-04-02 21:22:11 +0000

    www - rejestracja

---
+++ trunk/www/src/main/java/pl/umk/mat/zawodyweb/www/WWWHelper.java
@@ -0,0 +1,52 @@
+package pl.umk.mat.zawodyweb.www;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+/**
+ * This class contains helpful methods that are often used in beans
+ */
+public class WWWHelper {
+
+    /**
+     * Adds faces message to the given context.
+     *
+     * @param context
+     *            Faces context
+     * @param severity
+     *            Type of message
+     * @param comp
+     *            Component for which message should be added
+     * @param summary
+     *            Messages summary
+     * @param detail
+     *            Messages detail
+     */
+    public static void AddMessage(FacesContext context, Severity severity, UIComponent comp, String summary,
+            String detail) {
+        FacesMessage message = new FacesMessage(severity, summary, detail);
+        context.addMessage(comp.getClientId(context), message);
+    }
+
+    /**
+     * Adds faces message to the given context.
+     *
+     * @param context
+     *            Faces context
+     * @param severity
+     *            Type of message
+     * @param clientId
+     *            Client id of comonent for which message should be added
+     * @param summary
+     *            Messages summary
+     * @param detail
+     *            Messages detail
+     */
+    public static void AddMessage(FacesContext context, Severity severity, String clientId, String summary,
+            String detail) {
+        FacesMessage message = new FacesMessage(severity, summary, detail);
+        context.addMessage(clientId, message);
+    }
+}
