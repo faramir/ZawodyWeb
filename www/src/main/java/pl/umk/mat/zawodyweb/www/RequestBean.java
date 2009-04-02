3c29cc67045d2feef8742199a591e537fc1709e3
dalton <van.dalton@gmail.com>
2009-04-02 21:22:11 +0000

    www - rejestracja

--- www/src/main/java/pl/umk/mat/zawodyweb/www/RequestBean.java
+++ www/src/main/java/pl/umk/mat/zawodyweb/www/RequestBean.java
@@ -4,6 +4,13 @@
  */
 package pl.umk.mat.zawodyweb.www;
 
+import java.util.ResourceBundle;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.Transaction;
 import pl.umk.mat.zawodyweb.database.DAOFactory;
 import pl.umk.mat.zawodyweb.database.UsersDAO;
@@ -15,10 +22,9 @@ import pl.umk.mat.zawodyweb.database.pojo.Users;
  * @author slawek
  */
 public class RequestBean {
-
+    private final ResourceBundle messages = ResourceBundle.getBundle("pl.umk.mat.zawodyweb.www.Messages");
     private Users newUser = new Users();
     private String repPasswd;
-    private String captcha;
     
     /**
      * @return the newUser
@@ -49,6 +55,10 @@ public class RequestBean {
             t.commit();
         }catch(Exception e){
             t.rollback();
+            FacesContext context = FacesContext.getCurrentInstance();
+            String summary = messages.getString("login_exists");
+            WWWHelper.AddMessage(context, FacesMessage.SEVERITY_ERROR, "formRegister:login", summary, null);
+            newUser.setPass(StringUtils.EMPTY);
             return null;
         }
 
@@ -56,18 +66,24 @@ public class RequestBean {
     }
 
     /**
-     * @return the captcha
+     * Validates component with captcha text entered.
+     *
+     * @param context
+     *            faces context in which component resides
+     * @param component
+     *            component to be validated
+     * @param obj
+     *            data entered in component
      */
-    public String getCaptcha() {
-        return captcha;
+    public void validateCaptcha(FacesContext context, UIComponent component, Object obj) {
+        String captcha = (String) obj;
+        ExternalContext extContext = context.getExternalContext();
+        String tmp = (String) extContext.getSessionMap().get("captchaKey");
+
+        if (!captcha.toLowerCase().equals(tmp.toLowerCase())) {
+            ((HtmlInputText) component).setValid(false);
+            String summary = messages.getString("bad_captcha");
+            WWWHelper.AddMessage(context, FacesMessage.SEVERITY_ERROR, component, summary, null);
+        }
     }
-
-    /**
-     * @param captcha the captcha to set
-     */
-    public void setCaptcha(String captcha) {
-        this.captcha = captcha;
-    }
-
-
 }
