525c229a9d5dbb28f04a0d97623e415bf6b906ff
dalton <dalton@7685f17d-9d9b-4d2b-a256-61293067a286>
2009-04-07 21:01:12 +0000

    www - logowanie - zapamiętywanie loginu w ciachu

--- trunk/www/src/main/java/pl/umk/mat/zawodyweb/www/SessionBean.java
+++ trunk/www/src/main/java/pl/umk/mat/zawodyweb/www/SessionBean.java
@@ -7,6 +7,10 @@ package pl.umk.mat.zawodyweb.www;
 import java.util.ResourceBundle;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.Transaction;
 import pl.umk.mat.zawodyweb.database.DAOFactory;
 import pl.umk.mat.zawodyweb.database.UsersDAO;
@@ -22,6 +26,7 @@ public class SessionBean {
     private final ResourceBundle messages = ResourceBundle.getBundle("pl.umk.mat.zawodyweb.www.Messages");
     private Users currentUser = new Users();
     private boolean isLoggedIn;
+    private Boolean rememberMe;
 
     /**
      * @return the currentUser
@@ -38,6 +43,18 @@ public class SessionBean {
     }
 
     public String logIn() {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        Cookie cookie = new Cookie("login", currentUser.getLogin());
+        if (rememberMe) {
+            cookie.setMaxAge(999);
+        } else {
+            cookie.setMaxAge(0);
+        }
+        HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+        response.addCookie(cookie);
+
+
         Transaction t = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         try {
             UsersDAO dao = DAOFactory.DEFAULT.buildUsersDAO();
@@ -53,12 +70,49 @@ public class SessionBean {
         }
 
         if (!isLoggedIn) {
-            FacesContext context = FacesContext.getCurrentInstance();
             String summary = messages.getString("bad_login_data");
             WWWHelper.AddMessage(context, FacesMessage.SEVERITY_ERROR, "formLogin:login", summary, null);
             return null;
         }
 
-        return "index";
+        return "start";
+    }
+
+    private Cookie getLoginCookie() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        String viewId = context.getViewRoot().getViewId();
+        if (viewId.equals("/login.jspx")) {
+            HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("login")) {
+                    return cookie;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Boolean getRememberMe() {
+        return getLoginCookie() != null;
+    }
+
+    public void setRememberMe(Boolean value) {
+        rememberMe = value;
+    }
+
+    public String getLogin() {
+        String result;
+        try {
+            result = getLoginCookie().getValue();
+        } catch (NullPointerException e) {
+            result = StringUtils.EMPTY;
+        }
+
+        return result;
+    }
+
+    public void setLogin(String value) {
+        currentUser.setLogin(value);
     }
 }
