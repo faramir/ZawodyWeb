3eebeac6e4e521dc6023936a5273e7d10942b164
faramir <faramir@7685f17d-9d9b-4d2b-a256-61293067a286>
2009-05-10 16:36:57 +0000

    dodanie enuma dla wartości submits->result

---
+++ trunk/database/src/main/java/pl/umk/mat/zawodyweb/database/SubmitsResultEnum.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.umk.mat.zawodyweb.database;
+
+/**
+ *
+ * @author faramir
+ */
+public enum SubmitsResultEnum {
+
+    WAIT(0),
+    PROCESS(1),
+    DONE(2);
+    private final int code;
+
+    private SubmitsResultEnum(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static SubmitsResultEnum getByCode(int code) {
+        for (SubmitsResultEnum type : SubmitsResultEnum.values()) {
+            if (code == type.getCode()) {
+                return type;
+            }
+        }
+        return null;
+    }
+}
