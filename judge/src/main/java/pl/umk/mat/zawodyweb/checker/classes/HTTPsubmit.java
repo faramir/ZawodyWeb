fcdaf2a2df377cb802dd97a77f583b5b8fdd47f2
jakub <jprabucki@gmail.com>
2009-05-11 22:17:27 +0000

    OPSS Submit (w powijakach)

---
+++ judge/src/main/java/pl/umk/mat/zawodyweb/checker/classes/HTTPsubmit.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.umk.mat.zawodyweb.checker.classes;
+
+import pl.umk.mat.zawodyweb.checker.CheckerInterface;
+import pl.umk.mat.zawodyweb.checker.CheckerResult;
+import pl.umk.mat.zawodyweb.checker.TestInput;
+import pl.umk.mat.zawodyweb.checker.TestOutput;
+import pl.umk.mat.zawodyweb.compiler.Program;
+
+
+/**
+ *
+ * @author Jakub Prabucki
+ */
+public class HTTPsubmit implements CheckerInterface{
+
+    @Override
+    public CheckerResult check(Program program, TestInput input, TestOutput output) {
+        TestOutput to = program.runTest(input);
+        CheckerResult result =  new CheckerResult();
+        result.setResult(to.getResult());
+        result.setMemUsed(to.getMemUsed());
+        result.setRuntime(to.getRuntime());
+        result.setDecription(to.getResultDesc());
+        return result;
+    }
+}
