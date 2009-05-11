b3f303c36c5d242af246cdf59f2dd1c850a3365f
lukash2k <lukash2k@o2.pl>
2009-05-11 00:19:59 +0000

    Cos nie pyklo, teraz idzie update CheckerErrors oraz configuration.xml

--- database/src/main/java/pl/umk/mat/zawodyweb/database/CheckerErrors.java
+++ database/src/main/java/pl/umk/mat/zawodyweb/database/CheckerErrors.java
@@ -10,11 +10,13 @@ package pl.umk.mat.zawodyweb.database;
  */
 public class CheckerErrors {
 
-    public static int UNDEF = -1;
-    public static int ACC = 0;
-    public static int WA = 1;
-    public static int TLE = 2;
-    public static int CE = 3;
-    public static int RE = 4;
-    public static int MLE = 5;
+    public static final int UNDEF = -1;
+    public static final int ACC = 0;
+    public static final int WA = 1;
+    public static final int TLE = 2;
+    public static final int CE = 3;
+    public static final int RE = 4;
+    public static final int MLE = 5;
+    public static final int RV = 6;
+    public static final int CTLE = 7;
 }
