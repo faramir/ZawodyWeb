da9b212a457ff416fc953cd5ab4e71a1181861c4
jakub <jakub@7685f17d-9d9b-4d2b-a256-61293067a286>
2009-05-14 11:24:55 +0000

    logowanie do ACM nieskończone

--- trunk/judge/src/main/java/pl/umk/mat/zawodyweb/compiler/classes/LanguageACM.java
+++ trunk/judge/src/main/java/pl/umk/mat/zawodyweb/compiler/classes/LanguageACM.java
@@ -4,11 +4,17 @@
  */
 package pl.umk.mat.zawodyweb.compiler.classes;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 import pl.umk.mat.zawodyweb.checker.TestInput;
 import pl.umk.mat.zawodyweb.checker.TestOutput;
 import pl.umk.mat.zawodyweb.compiler.CompilerInterface;
@@ -35,6 +41,7 @@ public class LanguageACM implements CompilerInterface {
         String pass = "spamz2";
         HttpClient client = new HttpClient();
         GetMethod logging = new GetMethod(acmSite);
+        InputStream firstGet = null;
         try {
             client.executeMethod(logging);
         } catch (HttpException e) {
@@ -48,6 +55,45 @@ public class LanguageACM implements CompilerInterface {
             result.setResultDesc(e.getMessage());
             result.setText("IOException");
             logging.releaseConnection();
+        }
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(firstGet, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+        }
+        String line, name, value;
+        NameValuePair[] loginData = new NameValuePair[12];
+        loginData[0] = new NameValuePair("username", login);
+        loginData[1] = new NameValuePair("passwd", pass);
+        int noData = 2;
+        try {
+            line = br.readLine();
+            while (line != null && !line.matches(".*class=\"mod_login\".*")) {
+                line = br.readLine();
+            }
+            while (line != null && !line.matches(".*Submit.*Login.*")) {
+                if (line.matches(".*hidden.*name=\".*value=\".*")) {
+                    name = line.split("name=\"")[1].split("\"")[0];
+                    value = line.split("value=\"")[1].split("\"")[0];
+                    loginData[noData++] = new NameValuePair(name, value.replaceAll(":", "%3A"));
+                }
+                line = br.readLine();
+            }
+            loginData[noData++] = new NameValuePair("remember", "yes");
+            loginData[noData++] = new NameValuePair("Submit", "Login");
+        } catch (IOException e) {
+        }
+        PostMethod sendAnswer = new PostMethod("http://uva.onlinejudge.org/index.php?option=com_comprofiler&amp;task=login");
+        sendAnswer.setRequestHeader("Referer", acmSite);
+        sendAnswer.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; pl; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10 (.NET CLR 3.5.30729)");
+        sendAnswer.setRequestHeader("Connection", "Close");
+        sendAnswer.setRequestBody(loginData);
+        try {
+            client.executeMethod(sendAnswer);
+        } catch (HttpException e) {
+            sendAnswer.releaseConnection();
+        } catch (IOException e) {
+            sendAnswer.releaseConnection();
         }
         return result;
     }
