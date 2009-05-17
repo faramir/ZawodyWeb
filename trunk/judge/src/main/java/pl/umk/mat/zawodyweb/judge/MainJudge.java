3992430a5066fe3ac8bc967f51846f0614644174
lukash2k <lukash2k@7685f17d-9d9b-4d2b-a256-61293067a286>
2009-05-17 18:05:56 +0000

    MainJudge olog4jowany. Dalszych (znaczacych) zmian nie przewiduje.

--- trunk/judge/src/main/java/pl/umk/mat/zawodyweb/judge/MainJudge.java
+++ trunk/judge/src/main/java/pl/umk/mat/zawodyweb/judge/MainJudge.java
@@ -12,6 +12,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.List;
 import java.util.Properties;
+import org.apache.log4j.Logger;
 import org.hibernate.Transaction;
 import pl.umk.mat.zawodyweb.checker.CheckerInterface;
 import pl.umk.mat.zawodyweb.checker.CheckerResult;
@@ -20,6 +21,7 @@ import pl.umk.mat.zawodyweb.checker.TestOutput;
 import pl.umk.mat.zawodyweb.compiler.Code;
 import pl.umk.mat.zawodyweb.compiler.CompilerInterface;
 import pl.umk.mat.zawodyweb.compiler.Program;
+import pl.umk.mat.zawodyweb.database.CheckerErrors;
 import pl.umk.mat.zawodyweb.database.DAOFactory;
 import pl.umk.mat.zawodyweb.database.SubmitsResultEnum;
 import pl.umk.mat.zawodyweb.database.hibernate.HibernateUtil;
@@ -33,6 +35,9 @@ import pl.umk.mat.zawodyweb.database.pojo.Tests;
  * @author lukash2k
  */
 public class MainJudge {
+
+
+    public static final org.apache.log4j.Logger logger = Logger.getLogger(MainJudge.class);
 
     /**
      * @param args the command line arguments
@@ -54,10 +59,13 @@ public class MainJudge {
         properties.setProperty("COMPILE_TIMEOUT", "30000");
 
         try {
-        properties.loadFromXML(new FileInputStream(configFile));
+            logger.debug("Reading configuration file from "+configFile+"...");
+            properties.loadFromXML(new FileInputStream(configFile));
         } catch (Exception ex) {
-            
+            logger.fatal(ex.getMessage());
         }
+        logger.debug("Connection with JudgeManager on "+properties.getProperty("HOST")+
+                ":"+properties.getProperty("PORT")+"...");
         Socket sock = new Socket(InetAddress.getByName(properties.getProperty("HOST")),
                 Integer.parseInt(properties.getProperty("PORT")));
         DataInputStream input = new DataInputStream(
@@ -66,10 +74,13 @@ public class MainJudge {
         while (15 == 15) {
             int id;
             try {
-            id = input.readInt();
+                id = input.readInt();
+                logger.info("Received submit id: "+id);
             } catch (IOException ex) {
+                logger.error("Connection to JudgeManager closed, shutting down Judge...");
                 break;
             }
+            logger.debug("Setting submit status to PROCESS...");
             Transaction transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
             Submits submit = DAOFactory.DEFAULT.buildSubmitsDAO().getById(id);
             submit.setResult(SubmitsResultEnum.PROCESS.getCode());
@@ -77,32 +88,42 @@ public class MainJudge {
             transaction.commit();
             transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
             if (!sock.isConnected()) {
+                logger.error("Connection to JudgeManager closed, shutting down Judge...");
                 break;
             }
             byte[] codeText = submit.getCode();
             String filename = submit.getFilename();
-            if (filename!=null && !filename.isEmpty()) {
+            if (filename != null && !filename.isEmpty()) {
                 properties.setProperty("COMPILED_FILENAME", filename);
                 properties.setProperty("CODE_FILENAME", filename);
             }
+            logger.debug("Downloading compiler class...");
             Classes compilerClasses = submit.getLanguages().getClasses();
             CompilerInterface compiler = (CompilerInterface) new CompiledClassLoader().loadCompiledClass(compilerClasses.getFilename(),
                     compilerClasses.getCode()).newInstance();
             properties.setProperty("CODEFILE_EXTENSION", submit.getLanguages().getExtension());
             compiler.setProperties(properties);
+            logger.debug("Downloading diff class...");
             Classes diffClasses = submit.getProblems().getClasses();
             CheckerInterface checker = (CheckerInterface) new CompiledClassLoader().loadCompiledClass(diffClasses.getFilename(),
                     diffClasses.getCode()).newInstance();
             Code code = new Code(codeText, compiler);
             Program program = code.compile();
+            logger.debug("Downloading tests...");
             List<Tests> tests = submit.getProblems().getTestss();
             TestInput testInput;
             TestOutput testOutput;
+            boolean undefinedResult = false;
+            logger.debug("Starting tests...");
             for (Tests test : tests) {
                 testInput = new TestInput(test.getInput(),
                         test.getTimelimit(), submit.getProblems().getMemlimit());
                 testOutput = new TestOutput(test.getOutput());
                 CheckerResult result = checker.check(program, testInput, testOutput);
+                if (result.getResult() == CheckerErrors.UNDEF) {
+                    undefinedResult = true;
+                    break;
+                }
                 Results dbResult = new Results();
                 dbResult.setMemory(result.getMemUsed());
                 dbResult.setRuntime(result.getRuntime());
@@ -111,11 +132,20 @@ public class MainJudge {
                 dbResult.setSubmits(submit);
                 dbResult.setTests(test);
                 DAOFactory.DEFAULT.buildResultsDAO().save(dbResult);
+                logger.debug("Test finished.");
             }
+            logger.debug("All tests finished. Closing program.");
             program.closeProgram();
-            submit.setResult(SubmitsResultEnum.DONE.getCode());
-            DAOFactory.DEFAULT.buildSubmitsDAO().saveOrUpdate(submit);
-            transaction.commit();
+            if (!undefinedResult) {
+                logger.debug("Saving results to database.");
+                submit.setResult(SubmitsResultEnum.DONE.getCode());
+                DAOFactory.DEFAULT.buildSubmitsDAO().saveOrUpdate(submit);
+                transaction.commit();
+            } else {
+                logger.error("Some of the tests got UNDEFINED result -- this should not happend.");
+                transaction.rollback();
+            }
+            logger.info("Processing SubmitID: "+id+" finished.");
             output.writeInt(id);
         }
         HibernateUtil.getSessionFactory().getCurrentSession().close();
