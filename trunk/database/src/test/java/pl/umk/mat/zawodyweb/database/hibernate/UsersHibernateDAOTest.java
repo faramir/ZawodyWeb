5d6b50747c0caed9b3a073705d8a090a109ff495
faramir <faramir@7685f17d-9d9b-4d2b-a256-61293067a286>
2009-05-21 21:31:51 +0000

    Dodane LazyFetching do Hibernate'a ;) - test zadziałał, więc raczej działać będzie. Submits.Code jest pobierany leniwie. Zakomentowany test do sprawdzania czy na pewno leniwie.
    Aktualny wynik testu wygląda następująco:
    --- cut ---
    getSubmitById()
    Hibernate: select submits0_.id as id6_7_, submits0_.filename as filename6_7_, submits0_.result as result6_7_, submits0_.problemsid as problemsid6_7_, submits0_.notes as notes6_7_, submits0_.sdate as sdate6_7_, submits0_.languagesid as language8_6_7_, submits0_.usersid as usersid6_7_, problems1_.id as id8_0_, problems1_.pdf as pdf8_0_, problems1_.pdfid as pdfid8_0_, problems1_.abbrev as abbrev8_0_, problems1_.memlimit as memlimit8_0_, problems1_.seriesid as seriesid8_0_, problems1_.name as name8_0_, problems1_.classesid as classesid8_0_, problems1_.text as text8_0_, series2_.id as id0_1_, series2_.startdate as startdate0_1_, series2_.enddate as enddate0_1_, series2_.freezedate as freezedate0_1_, series2_.unfreezedate as unfreeze5_0_1_, series2_.penaltytime as penaltyt6_0_1_, series2_.contestsid as contestsid0_1_, series2_.name as name0_1_, contests3_.id as id7_2_, contests3_.startdate as startdate7_2_, contests3_.visibility as visibility7_2_, contests3_.about as about7_2_, contests3_.tech as tech7_2_, contests3_.name as name7_2_, contests3_.type as type7_2_, contests3_.rules as rules7_2_, classes4_.id as id5_3_, classes4_.filename as filename5_3_, classes4_.version as version5_3_, classes4_.description as descript4_5_3_, classes4_.code as code5_3_, languages5_.id as id2_4_, languages5_.name as name2_4_, languages5_.classesid as classesid2_4_, languages5_.extension as extension2_4_, classes6_.id as id5_5_, classes6_.filename as filename5_5_, classes6_.version as version5_5_, classes6_.description as descript4_5_5_, classes6_.code as code5_5_, users7_.id as id1_6_, users7_.firstname as firstname1_6_, users7_.lastname as lastname1_6_, users7_.email as email1_6_, users7_.birthdate as birthdate1_6_, users7_.login as login1_6_, users7_.pass as pass1_6_, users7_.school as school1_6_, users7_.tutor as tutor1_6_, users7_.emailnotification as emailno10_1_6_, users7_.address as address1_6_ from public.submits submits0_ left outer join public.problems problems1_ on submits0_.problemsid=problems1_.id left outer join public.series series2_ on problems1_.seriesid=series2_.id left outer join public.contests contests3_ on series2_.contestsid=contests3_.id left outer join public.classes classes4_ on problems1_.classesid=classes4_.id left outer join public.languages languages5_ on submits0_.languagesid=languages5_.id left outer join public.classes classes6_ on languages5_.classesid=classes6_.id left outer join public.users users7_ on submits0_.usersid=users7_.id where submits0_.id=?
    id = 8
    filename = dupa.cpp
    getCode()
    Hibernate: select submits_.code as code6_ from public.submits submits_ where submits_.id=?
    code = #include <cstdio>
    using namespace std;
    
    int euclid(int a, int b)
    {
      int tmp;
      while (b != 0)    /* dopoki b jest rozne od 0, */
      {
        tmp = a % b;    /* oblicz a modulo b, */
        a = b;          /* nadaj a wartosc b, */
        b = tmp;        /* nadaj b wartosc tmp */
      }
      return a;
    }
    
    int main()
    {
      for(int i=0;i<30;++i)
        if(euclid(i, 30) == 1)
          printf("%d ", i);
      printf("\n");
      return 0;
    }
    --- cut ---

--- trunk/database/src/test/java/pl/umk/mat/zawodyweb/database/hibernate/UsersHibernateDAOTest.java
+++ trunk/database/src/test/java/pl/umk/mat/zawodyweb/database/hibernate/UsersHibernateDAOTest.java
@@ -2,21 +2,17 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pl.umk.mat.zawodyweb.database.hibernate;
 
-import java.sql.Timestamp;
-import java.util.List;
 import org.hibernate.Transaction;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import pl.umk.mat.zawodyweb.database.DAOFactory;
-import pl.umk.mat.zawodyweb.database.SeriesDAO;
+import pl.umk.mat.zawodyweb.database.SubmitsDAO;
 import pl.umk.mat.zawodyweb.database.UsersDAO;
-import pl.umk.mat.zawodyweb.database.pojo.Series;
+import pl.umk.mat.zawodyweb.database.pojo.Submits;
 import static org.junit.Assert.*;
 import pl.umk.mat.zawodyweb.database.pojo.Users;
 
@@ -53,7 +49,7 @@ public class UsersHibernateDAOTest {
         System.out.println("getById");
         Transaction t = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         UsersDAO dao = HibernateDAOFactory.DEFAULT.buildUsersDAO();
-        Users user =new Users();
+        Users user = new Users();
         user.setId(0);
         user.setLogin("test_user");
         user.setPass("test_user");
@@ -65,4 +61,20 @@ public class UsersHibernateDAOTest {
         t.commit();
     }
 
+    /**
+     * Test of Lazy Connection to Submit.
+     */
+    /*@Test
+    public void testLazy() {
+        Transaction t = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+        SubmitsDAO dao = HibernateDAOFactory.DEFAULT.buildSubmitsDAO();
+        Submits submit = new Submits();
+        System.out.println("getSubmitById()");
+        submit = dao.getById(8);
+        System.out.println("id = " + submit.getId());
+        System.out.println("filename = " + submit.getFilename());
+        System.out.println("getCode()");
+        System.out.println("code = " + new String(submit.getCode()));
+        t.commit();
+    }*/
 }
