efc332c67d9b22a679595fad43b53c3f46bc53ce
jakub <jprabucki@gmail.com>
2009-05-31 14:46:17 +0000

    Kopiowanie treści zadania, dostępnych języków oraz testów. Zostało do napisania kopiowanie submitów

--- www/src/main/java/pl/umk/mat/zawodyweb/www/util/ProblemsUtils.java
+++ www/src/main/java/pl/umk/mat/zawodyweb/www/util/ProblemsUtils.java
@@ -1,7 +1,14 @@
 package pl.umk.mat.zawodyweb.www.util;
 
+import java.util.List;
+import java.util.ListIterator;
+import org.hibernate.Transaction;
+import pl.umk.mat.zawodyweb.database.DAOFactory;
+import pl.umk.mat.zawodyweb.database.hibernate.HibernateUtil;
+import pl.umk.mat.zawodyweb.database.pojo.LanguagesProblems;
 import pl.umk.mat.zawodyweb.database.pojo.Problems;
 import pl.umk.mat.zawodyweb.database.pojo.Series;
+import pl.umk.mat.zawodyweb.database.pojo.Tests;
 
 /**
  *
@@ -13,15 +20,51 @@ public class ProblemsUtils {
 
     private ProblemsUtils() {
     }
-    
-    public static ProblemsUtils getInstance(){
+
+    public static ProblemsUtils getInstance() {
         return instance;
     }
-    
-    public Problems copySolution(Problems problem, Series serie, boolean copySolution){
-        //TODO: implement this
-        return null;
+
+    public Problems copySolution(Problems problem, Series serie, boolean copySolution) {
+        Transaction transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+        Problems copyProblem = new Problems();
+        copyProblem.setAbbrev(problem.getAbbrev());
+        copyProblem.setClasses(problem.getClasses());
+        copyProblem.setMemlimit(problem.getMemlimit());
+        copyProblem.setName(problem.getName());
+        copyProblem.setPDF(problem.getPDF());
+        copyProblem.setSeries(serie);
+        copyProblem.setText(problem.getText());
+        DAOFactory.DEFAULT.buildProblemsDAO().saveOrUpdate(copyProblem);
+        List<LanguagesProblems> findByProblemsid = DAOFactory.DEFAULT.buildLanguagesProblemsDAO().findByProblemsid(problem.getId());
+        ListIterator<LanguagesProblems> iterator = findByProblemsid.listIterator();
+        while(iterator.hasNext()){
+            LanguagesProblems newLanguage = new LanguagesProblems();
+            LanguagesProblems oldLanguage = null;
+            oldLanguage = iterator.next();
+            newLanguage.setLanguages(oldLanguage.getLanguages());
+            newLanguage.setProblems(copyProblem);
+            DAOFactory.DEFAULT.buildLanguagesProblemsDAO().saveOrUpdate(newLanguage);
+        }
+        List<Tests> findByProblemsid1 = DAOFactory.DEFAULT.buildTestsDAO().findByProblemsid(problem.getId());
+        ListIterator<Tests> iterator1 = findByProblemsid1.listIterator();
+        while(iterator1.hasNext()){
+            Tests newTest = new Tests();
+            Tests oldTest = null;
+            oldTest = iterator1.next();
+            newTest.setInput(oldTest.getInput());
+            newTest.setMaxpoints(oldTest.getMaxpoints());
+            newTest.setOutput(oldTest.getOutput());
+            newTest.setProblems(copyProblem);
+            newTest.setTestorder(oldTest.getTestorder());
+            newTest.setTimelimit(oldTest.getTimelimit());
+            newTest.setVisibility(oldTest.getVisibility());
+            DAOFactory.DEFAULT.buildTestsDAO().saveOrUpdate(newTest);
+        }
+        transaction.commit();
+        return copyProblem;
     }
+
     public void reJudge() {
         //TODO: implement this
     }
