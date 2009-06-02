3208962b6aeb00ec2cec644fdc0b625ec9b6798a
faramir <faramir@users.noreply.github.com>
2009-06-02 23:57:40 +0000

    koszernie poprawiony zegar powinien teraz działać :)

---
+++ www/src/main/java/pl/umk/mat/zawodyweb/www/ServerTime.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.umk.mat.zawodyweb.www;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author faramir
+ */
+public class ServerTime extends HttpServlet {
+
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "private, no-cache, no-store, must-revalidate");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        PrintWriter out = response.getWriter();
+        out.write(Long.toString(System.currentTimeMillis()));
+        out.flush();
+        out.close();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Returns server time";
+    }// </editor-fold>
+}
--- www/src/main/webapp/media/javascript/getter.js
+++ www/src/main/webapp/media/javascript/getter.js
@@ -1,42 +1,43 @@
 function createRequestObject(){
 
-var req;
+    var req;
 
-if(window.XMLHttpRequest){
-//For Firefox, Safari, Opera
-req = new XMLHttpRequest();
-}
-else if(window.ActiveXObject){
-//For IE 5+
-req = new ActiveXObject("Microsoft.XMLHTTP");
-}
-else{
-//Error for an old browser
-alert('Your browser is not IE 5 or higher, or Firefox or Safari or Opera');
-}
+    if(window.XMLHttpRequest){
+        //For Firefox, Safari, Opera
+        req = new XMLHttpRequest();
+    }
+    else if(window.ActiveXObject){
+        //For IE 5+
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else{
+        //Error for an old browser
+        alert('Your browser is not IE 5 or higher, or Firefox or Safari or Opera');
+    }
 
-return req;
+    return req;
 }
 
 //Make the XMLHttpRequest Object
 var http = createRequestObject();
 var res = null;
 
-function sendRequest(method, url){
-if(method == 'get' || method == 'GET'){
-http.open(method,url);
-http.onreadystatechange = handleResponse;
-http.send(null);
-}
-	return res;
+function sendRequest(method, url, handler){
+    if(method == 'get' || method == 'GET'){
+        http.open(method,url);
+        if (typeof(handler)=='undefined') handler=handleResponse;
+        http.onreadystatechange = handler;
+        http.send(null);
+    }
+    return res;
 }
 
 function handleResponse(){
-if(http.readyState == 4 && http.status == 200){
-var response = http.responseText;
-if(response){
-	res = response;
-}
-}      
+    if(http.readyState == 4 && http.status == 200){
+        var response = http.responseText;
+        if(response){
+            res = response;
+        }
+    }
 }
 
