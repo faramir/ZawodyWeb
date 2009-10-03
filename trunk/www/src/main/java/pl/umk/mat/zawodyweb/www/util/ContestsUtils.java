b7cc90509f133fd2ee0add2d63d0b1587eb1dea0
faramir <faramir@7685f17d-9d9b-4d2b-a256-61293067a286>
2009-10-03 12:23:53 +0000

    Kolejna poprawka ReJudge'a

--- trunk/www/src/main/java/pl/umk/mat/zawodyweb/www/util/ContestsUtils.java
+++ trunk/www/src/main/java/pl/umk/mat/zawodyweb/www/util/ContestsUtils.java
@@ -1,10 +1,7 @@
 package pl.umk.mat.zawodyweb.www.util;
 
 import java.util.List;
-import org.hibernate.Transaction;
 import pl.umk.mat.zawodyweb.database.DAOFactory;
-import pl.umk.mat.zawodyweb.database.ProblemsDAO;
-import pl.umk.mat.zawodyweb.database.hibernate.HibernateUtil;
 import pl.umk.mat.zawodyweb.database.pojo.Contests;
 import pl.umk.mat.zawodyweb.database.pojo.Problems;
 import pl.umk.mat.zawodyweb.database.pojo.Series;
@@ -25,13 +22,9 @@ public class ContestsUtils {
     }
 
     public void reJudge(Contests contest) {
-        Transaction transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         List<Series> findByContestsid = DAOFactory.DEFAULT.buildSeriesDAO().findByContestsid(contest.getId());
-        transaction.commit();
         for (Series serie : findByContestsid) {
-            transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
             List<Problems> findBySeriesid = DAOFactory.DEFAULT.buildProblemsDAO().findBySeriesid(serie.getId());
-            transaction.commit();
             for (Problems problem : findBySeriesid) {
                 ProblemsUtils.getInstance().reJudge(problem);
             }
--- trunk/www/src/main/java/pl/umk/mat/zawodyweb/www/util/SubmitsUtils.java
+++ trunk/www/src/main/java/pl/umk/mat/zawodyweb/www/util/SubmitsUtils.java
@@ -1,10 +1,8 @@
 package pl.umk.mat.zawodyweb.www.util;
 
-import org.hibernate.Transaction;
 import pl.umk.mat.zawodyweb.database.DAOFactory;
 import pl.umk.mat.zawodyweb.database.ResultsDAO;
 import pl.umk.mat.zawodyweb.database.SubmitsResultEnum;
-import pl.umk.mat.zawodyweb.database.hibernate.HibernateUtil;
 import pl.umk.mat.zawodyweb.database.pojo.Results;
 import pl.umk.mat.zawodyweb.database.pojo.Submits;
 import pl.umk.mat.zawodyweb.www.JudgeManagerConnector;
@@ -25,14 +23,12 @@ public class SubmitsUtils {
     }
 
     public void reJudge(Submits submit) {
-        Transaction transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         submit.setResult(SubmitsResultEnum.WAIT.getCode());
         DAOFactory.DEFAULT.buildSubmitsDAO().saveOrUpdate(submit);
         ResultsDAO resultsDAO = DAOFactory.DEFAULT.buildResultsDAO();
         for (Results r : submit.getResultss()) { //resultsDAO.findBySubmitsid(submit.getId())) {
             resultsDAO.delete(r);
         }
-        transaction.commit();
 
         JudgeManagerConnector.getInstance().sentToJudgeManager(submit.getId());
     }
