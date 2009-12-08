355b8abd38937511ec48e80d3faf578e53170c7d
faramir <faramir@users.noreply.github.com>
2009-12-08 11:30:14 +0000

    Lepsze rozwiązanie problemu z jednoczesnym czytaniem wyjścia i pisaniem wyjścia.

--- judge/src/main/java/pl/umk/mat/zawodyweb/compiler/classes/LanguageCPP.java
+++ judge/src/main/java/pl/umk/mat/zawodyweb/compiler/classes/LanguageCPP.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
 import pl.umk.mat.zawodyweb.checker.TestInput;
@@ -20,6 +19,7 @@ import pl.umk.mat.zawodyweb.checker.TestOutput;
 import pl.umk.mat.zawodyweb.compiler.CompilerInterface;
 import pl.umk.mat.zawodyweb.database.CheckerErrors;
 import pl.umk.mat.zawodyweb.judge.InterruptTimer;
+import pl.umk.mat.zawodyweb.judge.ReaderEater;
 
 /**
  *
@@ -37,29 +37,6 @@ public class LanguageCPP implements CompilerInterface {
         this.properties = properties;
     }
 
-/*    class ProgramReaderThreader extends Thread {
-
-        private IOException ex;
-        private StringBuilder outputText = new StringBuilder();
-        private BufferedReader inputStream;
-
-        public ProgramReaderThreader(BufferedReader bufferedReader) {
-            inputStream = bufferedReader;
-        }
-
-        @Override
-        public void run() {
-            try {
-                String line;
-                while ((line = inputStream.readLine()) != null) {
-                    outputText.append(line + "\n");
-                }
-            } catch (IOException e) {
-                this.ex = e;
-            }
-        }
-    }
-*/
     @Override
     public TestOutput runTest(String path, TestInput input) {
         TestOutput output = new TestOutput(null);
@@ -74,7 +51,7 @@ public class LanguageCPP implements CompilerInterface {
         System.gc();
         List<String> command = Arrays.asList(path);
         if (!System.getProperty("os.name").toLowerCase().matches("(?s).*windows.*")) {
-            command = Arrays.asList("bash", "-c", "FILE=`mktemp`; ulimit -v " + (input.getMemoryLimit() * 1024) + " -t " + (5 + input.getTimeLimit() / 1000) + " && '" + path + "' > $FILE && cat $FILE && rm $FILE");
+            command = Arrays.asList("bash", "-c", "ulimit -v " + (input.getMemoryLimit() * 1024) + " -t " + (5 + input.getTimeLimit() / 1000) + " && '" + path + "'");
         } else {
             logger.error("OS without bash: " + System.getProperty("os.name") + ". Memory Limit check is off.");
         }
@@ -83,28 +60,28 @@ public class LanguageCPP implements CompilerInterface {
             timer = new InterruptTimer();
             Process p = new ProcessBuilder(command).start();
             long time = new Date().getTime();
-            StringBuilder outputText = new StringBuilder();
+            String outputText = "";
             try {
                 timer.schedule(Thread.currentThread(), input.getTimeLimit());
                 inputStream = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-//                ProgramReaderThreader outputEater = new ProgramReaderThreader(inputStream);
-//                outputEater.start();
+                ReaderEater readerEater = new ReaderEater(inputStream);
+                Thread threadReaderEater = new Thread(readerEater);
+                threadReaderEater.start();
 
                 BufferedWriter outputStream = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
                 outputStream.write(input.getText());
-                //outputStream.flush();
                 outputStream.close();
                 logger.debug("Waiting for program after " + (new Date().getTime() - time) + "ms.");
 
-                String line;
-                while ((line = inputStream.readLine()) != null) {
-                    outputText.append(line + "\n");
+                p.waitFor();
+                threadReaderEater.join();
+
+                if (readerEater.getException() != null) {
+                    throw readerEater.getException();
                 }
 
-                p.waitFor();
-//                outputText = outputEater.outputText;
-                logger.debug("Stopped waitingFor.");
+                outputText = readerEater.getOutputText();
             } catch (InterruptedException ex) {
                 timer.cancel();
                 p.destroy();
@@ -129,14 +106,16 @@ public class LanguageCPP implements CompilerInterface {
                     return output;
                 }
             } catch (java.lang.IllegalThreadStateException ex) {
+                logger.fatal("Fatal Exception", ex);
                 p.destroy();
                 output.setResult(CheckerErrors.RE);
-                output.setResultDesc("Abnormal Program termination.\nExit status: " + p.exitValue() + "\n");
+                output.setResultDesc("Abnormal Program termination.");
                 return output;
             }
-            output.setRuntime((int) (currentTime - time));
 
-            output.setText(outputText.toString());
+            output.setRuntime((int) (currentTime - time));
+            output.setText(outputText);
+
             p.destroy();
         } catch (Exception ex) {
             logger.fatal("Fatal Exception (timer may not be canceled)", ex);
@@ -151,25 +130,25 @@ public class LanguageCPP implements CompilerInterface {
     @Override
     public byte[] precompile(byte[] code) {
         String str = new String(code);
-        String forbiddenCalls = "__asm__ __asm asm access acct alarm brk chdir chown chroot clearerr clearerr_unlocked close " +
-                "confstr crypt ctermid daemon dup2 dup encrypt endusershell euidaccess execl execle execlp " +
-                "execv execve execvp _exit fchdir fchown fcloseall fclose fdatasync fdopen feof_unlocked " +
-                "ferror ferror_unlocked fexecve fflush_unlocked fgetc fgetc_unlocked fgetpos64 fgetpos " +
-                "fgets_unlocked fileno fileno_unlocked flockfile fmemopen fopen64 fopen fopencookie fork " +
-                "fpathconf fprintf fputc fputc_unlocked fputs fputs_unlocked fread fread_unlocked freopen64 " +
-                "freopen fscanf fseek fseeko64 fseeko fsetpos64 fsetpos ftell ftello64 ftello ftruncate64 " +
-                "ftruncate ftrylockfile funlockfile fwrite fwrite_unlocked getc getc_unlocked " +
-                "get_current_dir_name getcwd __getdelim getdelim getdomainname getegid geteuid getgid getgroups " +
-                "gethostid gethostname getlogin getlogin_r getpagesize getpass __getpgid getpgid " +
-                "getpgrp getpid getppid getsid getuid getusershell getw getwd group_member isatty lchown link " +
-                "lockf64 lockf lseek nice __off64t open open_memstream pathconf pause pclose pipe popen pread64 " +
-                "pread profil pthread_atfork pthread_ putc putc_unlocked putw pwrite64 pwrite read readlink " +
-                "remove rename revoke rewind rmdir sbrk setbuf setbuffer setdomainname setegid seteuid setgid " +
-                "sethostid sethostname setlinebuf setlogin setpgid setpgrp setregid setreuid setsid setuid " +
-                "setusershell setvbuf signal sleep swab symlink sync sysconf tcgetpgrp tcsetpgrp tempnam " +
-                "tmpfile64 tmpfile tmpnam tmpnam_r truncate64 truncate ttyname ttyname_r ttyslot ualarm ungetc " +
-                "unlink usleep vfork vfprintf vfscanf vhangup write system " +
-                "mkfifo";
+        String forbiddenCalls = "__asm__ __asm asm access acct alarm brk chdir chown chroot clearerr clearerr_unlocked close "
+                + "confstr crypt ctermid daemon dup2 dup encrypt endusershell euidaccess execl execle execlp "
+                + "execv execve execvp _exit fchdir fchown fcloseall fclose fdatasync fdopen feof_unlocked "
+                + "ferror ferror_unlocked fexecve fflush_unlocked fgetc fgetc_unlocked fgetpos64 fgetpos "
+                + "fgets_unlocked fileno fileno_unlocked flockfile fmemopen fopen64 fopen fopencookie fork "
+                + "fpathconf fprintf fputc fputc_unlocked fputs fputs_unlocked fread fread_unlocked freopen64 "
+                + "freopen fscanf fseek fseeko64 fseeko fsetpos64 fsetpos ftell ftello64 ftello ftruncate64 "
+                + "ftruncate ftrylockfile funlockfile fwrite fwrite_unlocked getc getc_unlocked "
+                + "get_current_dir_name getcwd __getdelim getdelim getdomainname getegid geteuid getgid getgroups "
+                + "gethostid gethostname getlogin getlogin_r getpagesize getpass __getpgid getpgid "
+                + "getpgrp getpid getppid getsid getuid getusershell getw getwd group_member isatty lchown link "
+                + "lockf64 lockf lseek nice __off64t open open_memstream pathconf pause pclose pipe popen pread64 "
+                + "pread profil pthread_atfork pthread_ putc putc_unlocked putw pwrite64 pwrite read readlink "
+                + "remove rename revoke rewind rmdir sbrk setbuf setbuffer setdomainname setegid seteuid setgid "
+                + "sethostid sethostname setlinebuf setlogin setpgid setpgrp setregid setreuid setsid setuid "
+                + "setusershell setvbuf signal sleep swab symlink sync sysconf tcgetpgrp tcsetpgrp tempnam "
+                + "tmpfile64 tmpfile tmpnam tmpnam_r truncate64 truncate ttyname ttyname_r ttyslot ualarm ungetc "
+                + "unlink usleep vfork vfprintf vfscanf vhangup write system "
+                + "mkfifo";
         String strWithoutComments = new String();
         int len = str.length() - 1;
         try {
