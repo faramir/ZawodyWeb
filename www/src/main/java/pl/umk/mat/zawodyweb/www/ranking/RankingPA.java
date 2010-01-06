942de61f7f84741db67437beb594e91838d353cc
faramir <faramir@users.noreply.github.com>
2010-01-06 15:39:33 +0000

    poprawione generowanie rankingu - powinno być szybciej ;)

--- www/src/main/java/pl/umk/mat/zawodyweb/www/ranking/RankingPA.java
+++ www/src/main/java/pl/umk/mat/zawodyweb/www/ranking/RankingPA.java
@@ -124,57 +124,57 @@ public class RankingPA implements RankingInteface {
              * }
              */
             return 0;
-            }
+        }
 
-            @Override
-            public int compareTo(Object o) {
+        @Override
+        public int compareTo(Object o) {
             UserKI u2 = (UserKI) o;
 
             int r;
             r = comparePlace(u2);
             if (r != 0) {
-            return r;
+                return r;
             }
 
             r = RankingUtils.compareStrings(this.lastname, u2.lastname);
             if (r != 0) {
-            return r;
+                return r;
             }
 
             r = RankingUtils.compareStrings(firstname, u2.firstname);
             if (r != 0) {
-            return r;
+                return r;
             }
 
             return RankingUtils.compareStrings(this.login, u2.login);
-            }
-            }
+        }
+    }
 
-            private RankingTable getRankingKI(int contest_id, Timestamp checkDate, boolean admin, Integer series_id) {
-            Session hibernateSession = HibernateUtil.getSessionFactory().getCurrentSession();
+    private RankingTable getRankingKI(int contest_id, Timestamp checkDate, boolean admin, Integer series_id) {
+        Session hibernateSession = HibernateUtil.getSessionFactory().getCurrentSession();
 
-            Timestamp checkTimestamp;
+        Timestamp checkTimestamp;
 
-            UsersDAO usersDAO = DAOFactory.DEFAULT.buildUsersDAO();
-            SeriesDAO seriesDAO = DAOFactory.DEFAULT.buildSeriesDAO();
-            ProblemsDAO problemsDAO = DAOFactory.DEFAULT.buildProblemsDAO();
-            HashMap<Integer, UserKI> mapUserKI = new HashMap<Integer, UserKI>();
-            ArrayList<ProblemsKI> vectorProblemsKI = new ArrayList<ProblemsKI>();
+        UsersDAO usersDAO = DAOFactory.DEFAULT.buildUsersDAO();
+        SeriesDAO seriesDAO = DAOFactory.DEFAULT.buildSeriesDAO();
+        ProblemsDAO problemsDAO = DAOFactory.DEFAULT.buildProblemsDAO();
+        HashMap<Integer, UserKI> mapUserKI = new HashMap<Integer, UserKI>();
+        ArrayList<ProblemsKI> vectorProblemsKI = new ArrayList<ProblemsKI>();
 
-            boolean allTests;
-            boolean frozenRanking = false;
-            boolean frozenSeria;
+        boolean allTests;
+        boolean frozenRanking = false;
+        boolean frozenSeria;
 
-            long lCheckDate = checkDate.getTime();
+        long lCheckDate = checkDate.getTime();
 
-            for (Series series : seriesDAO.findByContestsid(contest_id)) {
+        for (Series series : seriesDAO.findByContestsid(contest_id)) {
 
             if (series_id != null && !series_id.equals(series.getId())) {
-            continue;
+                continue;
             }
 
             if (series.getStartdate().getTime() > lCheckDate) {
-            continue;
+                continue;
             }
 
             frozenSeria = false;
@@ -182,137 +182,137 @@ public class RankingPA implements RankingInteface {
             allTests = admin;
 
             if (!admin && series.getFreezedate() != null) {
-            if (lCheckDate > series.getFreezedate().getTime() && (series.getUnfreezedate() == null || lCheckDate < series.getUnfreezedate().getTime())) {
-            checkTimestamp = new Timestamp(series.getFreezedate().getTime());
-            if (series.getUnfreezedate() != null) {
-            frozenRanking = true;
-            }
-            frozenSeria = true;
-            }
+                if (lCheckDate > series.getFreezedate().getTime() && (series.getUnfreezedate() == null || lCheckDate < series.getUnfreezedate().getTime())) {
+                    checkTimestamp = new Timestamp(series.getFreezedate().getTime());
+                    if (series.getUnfreezedate() != null) {
+                        frozenRanking = true;
+                    }
+                    frozenSeria = true;
+                }
             }
 
             if (series.getUnfreezedate() != null) {
-            if (checkDate.after(series.getUnfreezedate())) {
-            allTests = true;
-            }
+                if (checkDate.after(series.getUnfreezedate())) {
+                    allTests = true;
+                }
             }
 
             for (Problems problems : problemsDAO.findBySeriesid(series.getId())) {
-            vectorProblemsKI.add(new ProblemsKI(problems.getId(), series.getStartdate().getTime(), problems.getAbbrev(), problems.getName(), frozenSeria));
+                vectorProblemsKI.add(new ProblemsKI(problems.getId(), series.getStartdate().getTime(), problems.getAbbrev(), problems.getName(), frozenSeria));
 
-            Query query = null;
-            if (allTests == true) {
-            query = hibernateSession.createSQLQuery("" +
-            "select usersid, sum(points) " +
-            "from submits,results,tests " +
-            "where submits.problemsid='" + problems.getId() + "' " +
-            "  and submits.id=results.submitsid " +
-            "  and tests.id=results.testsid" +
-            "  and sdate in ( " +
-            "        select max(sdate) " +
-            "	     from submits " +
-            "        where submits.problemsid='" + problems.getId() + "' " +
-            "          and submits.result='" + SubmitsResultEnum.DONE.getCode() + "' " +
-            "          and sdate <= '" + checkTimestamp.toString() + "' " +
-            //"	       and tests.visibility=1 " +
-            "	     group by usersid " +
-            "      ) " +
-            "group by usersid, submits.id");
-            } else {
-            query = hibernateSession.createSQLQuery("" +
-            "select usersid, sum(points) " +
-            "from submits,results,tests " +
-            "where submits.problemsid='" + problems.getId() + "' " +
-            "  and submits.id=results.submitsid " +
-            "  and tests.id=results.testsid" +
-            "  and sdate in ( " +
-            "        select max(sdate) " +
-            "	     from submits " +
-            "        where submits.problemsid='" + problems.getId() + "' " +
-            "          and submits.result='" + SubmitsResultEnum.DONE.getCode() + "' " +
-            "          and sdate <= '" + checkTimestamp.toString() + "' " +
-            //"	       and tests.visibility=1 " + // FIXME: with this, generating is veeery slow
-            "	     group by usersid " +
-            "      ) " +
-            "group by usersid, submits.id");
-            }
-
-            for (Object list : query.list()) {
-            Object[] o = (Object[]) list; // 0 - user.id, 1 - sum(points)
-
-            UserKI user = mapUserKI.get((Integer) o[0]);
-            if (user == null) {
-            Integer user_id = (Integer) o[0];
-            Users users = usersDAO.getById(user_id);
-            user = new UserKI(user_id, users);
-            mapUserKI.put((Integer) o[0], user);
-            }
-
-            user.add(problems.getId(), ((Number) o[1]).intValue());
-            }
-            }
-            }
-
-            /* Tworzenie rankingu z danych */
-            ArrayList<UserKI> cre = new ArrayList<UserKI>();
-            cre.addAll(mapUserKI.values());
-            Collections.sort(cre);
-
-            Collections.sort(vectorProblemsKI);
-
-            /* nazwy kolumn i CSSy */
-            ArrayList<String> columnsCSS = new ArrayList<String>();
-            ArrayList<String> columnsCaptions = new ArrayList<String>();
-            for (ProblemsKI problemsKI : vectorProblemsKI) {
-                columnsCaptions.add(RankingUtils.formatText(problemsKI.abbrev, problemsKI.name, problemsKI.frozen ? "frozen" : null));
-                columnsCSS.add("small");
-            }
-            columnsCaptions.add(messages.getString("points"));
-            columnsCSS.add("small");
-
-            /* tabelka z rankingiem */
-            ArrayList<RankingEntry> vectorRankingEntry = new ArrayList<RankingEntry>();
-            int place = 1;
-            UserKI userTmp = cre.get(0);
-            for (UserKI user : cre) {
-                if (userTmp.comparePlace(user) != 0) {
-                    ++place;
-                    userTmp = user;
+                Query query = null;
+                if (allTests == true) {
+                    query = hibernateSession.createSQLQuery(""
+                            + "select usersid, sum(points) "
+                            + "from submits,results,tests "
+                            + "where submits.problemsid='" + problems.getId() + "' "
+                            + "  and submits.id=results.submitsid "
+                            + "  and tests.id=results.testsid"
+                            + "  and sdate in ( "
+                            + "        select max(sdate) "
+                            + "	     from submits "
+                            + "        where submits.problemsid='" + problems.getId() + "' "
+                            + "          and submits.result='" + SubmitsResultEnum.DONE.getCode() + "' "
+                            + "          and sdate <= '" + checkTimestamp.toString() + "' "
+                            + //"	       and tests.visibility=1 " +
+                            "	     group by usersid "
+                            + "      ) "
+                            + "group by usersid, submits.id");
+                } else {
+                    query = hibernateSession.createSQLQuery(""
+                            + "select usersid, sum(points) "
+                            + "from submits,results,tests "
+                            + "where submits.problemsid='" + problems.getId() + "' "
+                            + "  and submits.id=results.submitsid "
+                            + "  and tests.id=results.testsid"
+                            + "	 and tests.visibility=1 " // FIXME: should be ok
+                            + "  and sdate in ( "
+                            + "        select max(sdate) "
+                            + "	     from submits "
+                            + "        where submits.problemsid='" + problems.getId() + "' "
+                            + "          and submits.result='" + SubmitsResultEnum.DONE.getCode() + "' "
+                            + "          and sdate <= '" + checkTimestamp.toString() + "' "
+                            + "	     group by usersid "
+                            + "      ) "
+                            + "group by usersid, submits.id");
                 }
-                ArrayList<String> v = new ArrayList<String>();
-                for (ProblemsKI problemsKI : vectorProblemsKI) {
-                    Integer solution_points = user.solutions.get(problemsKI.id_problem);
-                    if (solution_points == null) {
-                        v.add("-");
-                    } else {
-                        v.add(solution_points.toString());
+
+                for (Object list : query.list()) {
+                    Object[] o = (Object[]) list; // 0 - user.id, 1 - sum(points)
+
+                    UserKI user = mapUserKI.get((Integer) o[0]);
+                    if (user == null) {
+                        Integer user_id = (Integer) o[0];
+                        Users users = usersDAO.getById(user_id);
+                        user = new UserKI(user_id, users);
+                        mapUserKI.put((Integer) o[0], user);
                     }
+
+                    user.add(problems.getId(), ((Number) o[1]).intValue());
                 }
-                v.add(Integer.toString(user.points));
-
-                vectorRankingEntry.add(new RankingEntry(place, user.formatName(), v));
             }
-
-            return new RankingTable(columnsCaptions, columnsCSS, vectorRankingEntry, frozenRanking);
         }
 
-        @Override
-        public RankingTable getRanking(int contest_id, Timestamp checkDate) {
-            return getRankingKI(contest_id, checkDate, false, null);
+        /* Tworzenie rankingu z danych */
+        ArrayList<UserKI> cre = new ArrayList<UserKI>();
+        cre.addAll(mapUserKI.values());
+        Collections.sort(cre);
+
+        Collections.sort(vectorProblemsKI);
+
+        /* nazwy kolumn i CSSy */
+        ArrayList<String> columnsCSS = new ArrayList<String>();
+        ArrayList<String> columnsCaptions = new ArrayList<String>();
+        for (ProblemsKI problemsKI : vectorProblemsKI) {
+            columnsCaptions.add(RankingUtils.formatText(problemsKI.abbrev, problemsKI.name, problemsKI.frozen ? "frozen" : null));
+            columnsCSS.add("small");
+        }
+        columnsCaptions.add(messages.getString("points"));
+        columnsCSS.add("small");
+
+        /* tabelka z rankingiem */
+        ArrayList<RankingEntry> vectorRankingEntry = new ArrayList<RankingEntry>();
+        int place = 1;
+        UserKI userTmp = cre.get(0);
+        for (UserKI user : cre) {
+            if (userTmp.comparePlace(user) != 0) {
+                ++place;
+                userTmp = user;
+            }
+            ArrayList<String> v = new ArrayList<String>();
+            for (ProblemsKI problemsKI : vectorProblemsKI) {
+                Integer solution_points = user.solutions.get(problemsKI.id_problem);
+                if (solution_points == null) {
+                    v.add("-");
+                } else {
+                    v.add(solution_points.toString());
+                }
+            }
+            v.add(Integer.toString(user.points));
+
+            vectorRankingEntry.add(new RankingEntry(place, user.formatName(), v));
         }
 
-        @Override
-        public RankingTable getRankingForAdmin(int contest_id, Timestamp checkDate) {
-            return getRankingKI(contest_id, checkDate, true, null);
-        }
-
-        @Override
-        public RankingTable getRankingForSeries(int contest_id, int series_id, Timestamp checkDate) {
-            return getRankingKI(contest_id, checkDate, false, series_id);
-        }
-
-        @Override
-        public RankingTable getRankingForSeriesForAdmin(int contest_id, int series_id, Timestamp checkDate) {
-            return getRankingKI(contest_id, checkDate, true, series_id);
-        }
+        return new RankingTable(columnsCaptions, columnsCSS, vectorRankingEntry, frozenRanking);
     }
+
+    @Override
+    public RankingTable getRanking(int contest_id, Timestamp checkDate) {
+        return getRankingKI(contest_id, checkDate, false, null);
+    }
+
+    @Override
+    public RankingTable getRankingForAdmin(int contest_id, Timestamp checkDate) {
+        return getRankingKI(contest_id, checkDate, true, null);
+    }
+
+    @Override
+    public RankingTable getRankingForSeries(int contest_id, int series_id, Timestamp checkDate) {
+        return getRankingKI(contest_id, checkDate, false, series_id);
+    }
+
+    @Override
+    public RankingTable getRankingForSeriesForAdmin(int contest_id, int series_id, Timestamp checkDate) {
+        return getRankingKI(contest_id, checkDate, true, series_id);
+    }
+}
