08fcbc10d14f4e9a33defffb729d1f1f7bb60be9
faramir <faramir@7685f17d-9d9b-4d2b-a256-61293067a286>
2011-01-02 23:37:55 +0000

    Dodanie manualnego sprawdzania rozwiązań oraz ograniczenia na liczbę znaków w niektórych polach.

---
+++ trunk/judge/src/main/java/pl/umk/mat/zawodyweb/checker/classes/ManualCheck.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.umk.mat.zawodyweb.checker.classes;
+
+import pl.umk.mat.zawodyweb.checker.CheckerInterface;
+import pl.umk.mat.zawodyweb.checker.CheckerResult;
+import pl.umk.mat.zawodyweb.checker.TestInput;
+import pl.umk.mat.zawodyweb.checker.TestOutput;
+import pl.umk.mat.zawodyweb.compiler.Program;
+import pl.umk.mat.zawodyweb.database.CheckerErrors;
+
+/**
+ *
+ * @author Jakub Prabucki
+ */
+public class ManualCheck implements CheckerInterface {
+
+    @Override
+    public CheckerResult check(Program program, TestInput input, TestOutput output) {
+        CheckerResult result = new CheckerResult();
+        result.setResult(CheckerErrors.MANUAL);
+        result.setMemUsed(0);
+        result.setRuntime(0);
+        result.setDescription("");
+        result.setPoints(0);
+        return result;
+    }
+}
