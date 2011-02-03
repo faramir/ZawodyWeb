6a6f1a2d2660b8b8f58ad7991a14431c24945c1f
faramir <faramir@7685f17d-9d9b-4d2b-a256-61293067a286>
2011-02-03 17:48:21 +0000

    Wyszukiwanie użytkownika na liście wszystkich użytkowników. Kilka zmian związanych z "bezpieczeństwem". Rozdzielenie XML Schema na kilka plików.

--- trunk/www/src/main/webapp/media/javascript/script.js
+++ trunk/www/src/main/webapp/media/javascript/script.js
@@ -1,24 +1,30 @@
-function confirm(e, args)
-{
-    jConfirm('Usun\u0105\u0107 '+ args.nazwa + '?', 'Usuwanie', function(r){if(r){window.location=args.link;}});
+function confirm(e,args){
+    jConfirm('Usun\u0105\u0107 '+args.nazwa+'?','Usuwanie',function(r){
+        if(r)window.location=args.link;
+    });
 }
-
-function confirm_rejudge(e, args)
-{
-    jConfirm('Sprawdzi\u0107 ponownie '+ args.nazwa + '?', 'reJudge', function(r){if(r){window.location=args.link;}});
+function confirm_rejudge(e,args){
+    jConfirm('Sprawdzi\u0107 ponownie '+args.nazwa+'?','reJudge',function(r){
+        if(r)window.location=args.link;
+    });
 }
-
-function prompt(e, args)
-{
-    jPrompt('Filtrowanie wg '+args.nazwa+'. Podaj warto\u015b\u0107: ', args.def, 'Filtrowanie', function(r){if(r){window.location=args.link+r+'.html';}});
+function confirm_ghost(e,args){
+    jConfirm('Ustawi\u0107 '+args.nazwa+' na '+args.widocznosc+'?','Ghost',function(r){
+        if(r)window.location=args.link;
+    });
 }
-
-function prompt_ranking(e, args)
-{
-    jPrompt('Poka\u017c ranking z daty: ', args.def, 'Poka\u017c ranking', function(r){if(r){window.location=args.link+r+'.html';}});
+function prompt(e,args){
+    jPrompt('Filtrowanie wg '+args.nazwa+'. Podaj warto\u015b\u0107: ',args.def,'Filtrowanie',function(r){
+        if(r)window.location=args.link+r+'.html';
+    });
 }
-
-function confirm_ghost(e, args)
-{
-    jConfirm('Ustawi\u0107 '+ args.nazwa + ' na ' + args.widocznosc + '?', 'Ghost', function(r){if(r){window.location=args.link;}});
+function prompt_ranking(e,args){
+    jPrompt('Poka\u017c ranking z daty: ',args.def,'Poka\u017c ranking',function(r){
+        if(r)window.location=args.link+r+'.html';
+    });
+}
+function prompt_user(e,args){
+    jPrompt('Podaj login: ',args.def,'Edycja u\u017cytkownika',function(r){
+        if(r)window.location=args.link+r+args.suffix+'.html';
+    });
 }
