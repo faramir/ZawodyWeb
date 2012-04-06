9fce3ee1399117f40b9229c131abd03a7ce9f7cd
faramir <faramir@users.noreply.github.com>
2012-04-06 15:19:32 +0000

    Przeniesienie konfiguracji do osobnych plików.

--- database/src/main/java/pl/umk/mat/zawodyweb/database/hibernate/HibernateUtil.java
+++ database/src/main/java/pl/umk/mat/zawodyweb/database/hibernate/HibernateUtil.java
@@ -1,39 +1,37 @@
 package pl.umk.mat.zawodyweb.database.hibernate;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
 
 /**
  * Generated at Fri May 08 19:01:00 CEST 2009
  *
  * @author Salto-db Generator v1.1 / EJB3 + Hibernate DAO
+ * @author Marek Nowicki
  */
 public final class HibernateUtil {
 
-	private static SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory;
 
-	static {
-		try {
-			sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-		} catch (Throwable ex) {
+    static {
+        try {
+            sessionFactory = new AnnotationConfiguration().configure().
+                    configure("/mappings.cfg.xml").
+                    buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+    private HibernateUtil() {
+    }
 
-	private HibernateUtil() {
-
-	}
-
-	/**
-	 * Returns the SessionFactory used for this static class.
-	 * 
-	 * @return SessionFactory
-	 */
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-
+    /**
+     * Returns the SessionFactory used for this static class.
+     *
+     * @return SessionFactory
+     */
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
