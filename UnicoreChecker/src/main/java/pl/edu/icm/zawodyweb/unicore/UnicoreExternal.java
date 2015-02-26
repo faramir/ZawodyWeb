15b4eab79879430a1b2c8d3ff32816261047d085
Marek Nowicki <faramir2@gmail.com>
2015-02-27 00:51:04 +0100

    Dodanie usuwanie plików wynikowych stworzonych przez UnicoreExternal.

--- UnicoreChecker/src/main/java/pl/edu/icm/zawodyweb/unicore/UnicoreExternal.java
+++ UnicoreChecker/src/main/java/pl/edu/icm/zawodyweb/unicore/UnicoreExternal.java
@@ -188,253 +188,284 @@ public class UnicoreExternal implements ExternalInterface {
 
     private void updateOutputWhenFailed(TestOutput output, String externalId) {
         List<String> files = getOutputFiles(externalId);
-        String outputText = null;
-        for (String stdoutPath : files) {
-            if (stdoutPath.contains("stderr")) {
-                try {
-                    outputText = new String(Files.readAllBytes(Paths.get(stdoutPath)), StandardCharsets.UTF_8);
-                    break;
-                } catch (IOException ex) {
-                    logger.error("Unable read stderr file", ex);
-                }
-            }
-        }
-        if (outputText == null) {
-            logger.info("No readable stderr file?");
-            output.setStatus(ResultsStatusEnum.UNKNOWN.getCode());
-
-            List<String> command = Arrays.asList(properties.getProperty("UNICORECC_BIN_PATH"), "wsrf", "getproperties", externalId);
-
-            String xmlText = executeCommand(command);
-            String logText = extractLogFromXml(xmlText);
-
-            if (logText != null) {
-                String[] lines = logText.split("[\\r\\n]+");
-                int i = lines.length;
-                while (i > 0) {
-                    String line = lines[--i];
-                    logger.trace("> " + line);
-                    if (line.contains("Processing failed, aborting")) {
+        try {
+            String outputText = null;
+            for (String stdoutPath : files) {
+                if (stdoutPath.contains("stderr")) {
+                    try {
+                        outputText = new String(Files.readAllBytes(Paths.get(stdoutPath)), StandardCharsets.UTF_8);
                         break;
+                    } catch (IOException ex) {
+                        logger.error("Unable read stderr file", ex);
                     }
                 }
-                String notes = "";
-                while (i < lines.length) {
-                    notes += lines[i++] + "\n";
+            }
+            if (outputText == null) {
+                logger.info("No readable stderr file?");
+                output.setStatus(ResultsStatusEnum.UNKNOWN.getCode());
+
+                List<String> command = Arrays.asList(properties.getProperty("UNICORECC_BIN_PATH"), "wsrf", "getproperties", externalId);
+
+                String xmlText = executeCommand(command);
+                String logText = extractLogFromXml(xmlText);
+
+                if (logText != null) {
+                    String[] lines = logText.split("[\\r\\n]+");
+                    int i = lines.length;
+                    while (i > 0) {
+                        String line = lines[--i];
+                        logger.trace("> " + line);
+                        if (line.contains("Processing failed, aborting")) {
+                            break;
+                        }
+                    }
+                    String notes = "";
+                    while (i < lines.length) {
+                        notes += lines[i++] + "\n";
+                    }
+                    output.setNotes(notes);
                 }
-                output.setNotes(notes);
+
+                return;
             }
 
-            return;
-        }
-
-        String[] lines = outputText.split("[\\r\\n]+");
-        int i = lines.length;
-        while (i > 0) {
-            String line = lines[--i];
-            logger.trace("> " + line);
-            if (line.startsWith("slurmstepd:") == false) {
-                ++i;
-                break;
+            String[] lines = outputText.split("[\\r\\n]+");
+            int i = lines.length;
+            while (i > 0) {
+                String line = lines[--i];
+                logger.trace("> " + line);
+                if (line.startsWith("slurmstepd:") == false) {
+                    ++i;
+                    break;
+                }
             }
-        }
-        String notes = "";
-        output.setStatus(ResultsStatusEnum.UNKNOWN.getCode());
-        while (i < lines.length) {
-            String line = lines[i++];
-            logger.trace(line);
-            if (line.contains("exceeded memory limit")) {
-                logger.debug("Job exceeded memory limit");
-                output.setStatus(ResultsStatusEnum.MLE.getCode());
-            } else if (line.contains("DUE TO TIME LIMIT")) {
-                logger.debug("Job exceeded time limit");
-                output.setStatus(ResultsStatusEnum.TLE.getCode());
+            String notes = "";
+            output.setStatus(ResultsStatusEnum.UNKNOWN.getCode());
+            while (i < lines.length) {
+                String line = lines[i++];
+                logger.trace(line);
+                if (line.contains("exceeded memory limit")) {
+                    logger.debug("Job exceeded memory limit");
+                    output.setStatus(ResultsStatusEnum.MLE.getCode());
+                } else if (line.contains("DUE TO TIME LIMIT")) {
+                    logger.debug("Job exceeded time limit");
+                    output.setStatus(ResultsStatusEnum.TLE.getCode());
+                }
+                notes += line + "\n";
             }
-            notes += line + "\n";
+            output.setNotes(notes);
+        } finally {
+            deleteFiles(files);
         }
-        output.setNotes(notes);
     }
 
     private void updateOutputWhenSuccess(TestOutput output, String externalId, TestInput testInput, TestOutput testOutput) {
         List<String> files = getOutputFiles(externalId);
-        String stdoutText = null;
-        String stderrText = null;
-        for (String filePath : files) {
-            if (stdoutText == null && filePath.contains("stdout")) {
-                try {
-                    stdoutText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
-                } catch (IOException ex) {
-                    logger.error("Unable read stdout file", ex);
+        try {
+            String stdoutText = null;
+            String stderrText = null;
+            for (String filePath : files) {
+                if (stdoutText == null && filePath.contains("stdout")) {
+                    try {
+                        stdoutText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+                    } catch (IOException ex) {
+                        logger.error("Unable read stdout file", ex);
+                    }
+                }
+                if (stderrText == null && filePath.contains("stderr")) {
+                    try {
+                        stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+                    } catch (IOException ex) {
+                        logger.error("Unable read stderr file", ex);
+                    }
                 }
             }
-            if (stderrText == null && filePath.contains("stderr")) {
-                try {
-                    stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
-                } catch (IOException ex) {
-                    logger.error("Unable read stderr file", ex);
+            if (stdoutText == null) {
+                logger.info("No readable stdout file?");
+                stdoutText = "";
+            }
+            if (stderrText == null) {
+                logger.info("No readable stderr file?");
+                stderrText = "";
+            }
+
+            String[] lines = stderrText.split("[\\r\\n]+");
+            int i = lines.length;
+            Pattern pattern = Pattern.compile("^### ELAPSED TIME IN NANOS: (?<nanos>[0-9]+) ###$");
+            while (i > 0) {
+                String line = lines[--i];
+                logger.trace("> " + line);
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.matches()) {
+                    String nanos = matcher.group("nanos");
+                    output.setRuntime((int) (Long.parseLong(nanos) / 1_000_000));
+                    break;
                 }
             }
-        }
-        if (stdoutText == null) {
-            logger.info("No readable stdout file?");
-            stdoutText = "";
-        }
-        if (stderrText == null) {
-            logger.info("No readable stderr file?");
-            stderrText = "";
-        }
 
-        String[] lines = stderrText.split("[\\r\\n]+");
-        int i = lines.length;
-        Pattern pattern = Pattern.compile("^### ELAPSED TIME IN NANOS: (?<nanos>[0-9]+) ###$");
-        while (i > 0) {
-            String line = lines[--i];
-            logger.trace("> " + line);
-            Matcher matcher = pattern.matcher(line);
-            if (matcher.matches()) {
-                String nanos = matcher.group("nanos");
-                output.setRuntime((int) (Long.parseLong(nanos) / 1_000_000));
-                break;
+            Scanner stdoutScanner = new Scanner(stdoutText);
+            Scanner testScanner = new Scanner(testOutput.getOutputText());
+            while (stdoutScanner.hasNext() && testScanner.hasNext()) {
+                String stdout = stdoutScanner.next();
+                String test = testScanner.next();
+                if (stdout.equals(test) == false) {
+                    output.setStatus(ResultsStatusEnum.WA.getCode());
+                    output.setPoints(0);
+                    return;
+                }
             }
-        }
-
-        Scanner stdoutScanner = new Scanner(stdoutText);
-        Scanner testScanner = new Scanner(testOutput.getOutputText());
-        while (stdoutScanner.hasNext() && testScanner.hasNext()) {
-            String stdout = stdoutScanner.next();
-            String test = testScanner.next();
-            if (stdout.equals(test) == false) {
+            if (stdoutScanner.hasNext() == testScanner.hasNext()) {
+                output.setStatus(ResultsStatusEnum.ACC.getCode());
+                output.setPoints(testInput.getMaxPoints());
+            } else {
                 output.setStatus(ResultsStatusEnum.WA.getCode());
                 output.setPoints(0);
-                return;
             }
-        }
-        if (stdoutScanner.hasNext() == testScanner.hasNext()) {
-            output.setStatus(ResultsStatusEnum.ACC.getCode());
-            output.setPoints(testInput.getMaxPoints());
-        } else {
-            output.setStatus(ResultsStatusEnum.WA.getCode());
-            output.setPoints(0);
+        } finally {
+            deleteFiles(files);
         }
     }
 
     private void updateOutputWhenRuntimeError(TestOutput output, String externalId) {
         List<String> files = getOutputFiles(externalId);
-        String stderrText = null;
-        for (String filePath : files) {
-            if (filePath.contains("stderr")) {
-                try {
-                    stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
-                    break;
-                } catch (IOException ex) {
-                    logger.error("Unable read stderr file", ex);
+        try {
+            String stderrText = null;
+            for (String filePath : files) {
+                if (filePath.contains("stderr")) {
+                    try {
+                        stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+                        break;
+                    } catch (IOException ex) {
+                        logger.error("Unable read stderr file", ex);
+                    }
                 }
             }
-        }
-        if (stderrText == null) {
-            logger.info("No readable stderr file?");
-            stderrText = "";
-        }
-
-        String[] lines = stderrText.split("[\\r\\n]+");
-        int i = lines.length;
-        Pattern pattern = Pattern.compile("^### ELAPSED TIME IN NANOS: (?<nanos>[0-9]+) ###$");
-        while (i > 0) {
-            String line = lines[--i];
-            logger.trace("> " + line);
-            Matcher matcher = pattern.matcher(line);
-            if (matcher.matches()) {
-                String nanos = matcher.group("nanos");
-                output.setRuntime((int) (Long.parseLong(nanos) / 1_000_000));
-                break;
+            if (stderrText == null) {
+                logger.info("No readable stderr file?");
+                stderrText = "";
             }
+
+            String[] lines = stderrText.split("[\\r\\n]+");
+            int i = lines.length;
+            Pattern pattern = Pattern.compile("^### ELAPSED TIME IN NANOS: (?<nanos>[0-9]+) ###$");
+            while (i > 0) {
+                String line = lines[--i];
+                logger.trace("> " + line);
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.matches()) {
+                    String nanos = matcher.group("nanos");
+                    output.setRuntime((int) (Long.parseLong(nanos) / 1_000_000));
+                    break;
+                }
+            }
+        } finally {
+            deleteFiles(files);
         }
     }
 
     private void updateOutputWhenCompileError(TestOutput output, String externalId) {
         List<String> files = getOutputFiles(externalId);
-        String stderrText = null;
-        for (String filePath : files) {
-            if (filePath.contains("stderr")) {
-                try {
-                    stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
-                    break;
-                } catch (IOException ex) {
-                    logger.error("Unable read stderr file", ex);
-                }
-            }
-        }
-        if (stderrText == null) {
-            logger.info("No readable stderr file?");
-            stderrText = "";
-        }
-
-        String[] lines = stderrText.split("[\\r\\n]+");
-        int stop = -1;
-        int i = lines.length;
-        Pattern pattern = Pattern.compile("^### ERROR: COMPILER_ERROR: (?:[0-9]+) ###$");
-        while (i > 0) {
-            String line = lines[--i];
-            logger.trace("> " + line);
-            if (stop < 0) {
-                Matcher matcher = pattern.matcher(line);
-                if (matcher.matches()) {
-                    stop = i;
-                }
-            } else {
-                if ("### COMPILE FILES ###".equals(line)) {
-                    String notes = "";
-                    for (int j = i + 1; j < stop; ++j) {
-                        notes += lines[j] + "\n";
+        try {
+            String stderrText = null;
+            for (String filePath : files) {
+                if (filePath.contains("stderr")) {
+                    try {
+                        stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+                        break;
+                    } catch (IOException ex) {
+                        logger.error("Unable read stderr file", ex);
                     }
-                    output.setNotes(notes);
-                    return;
                 }
             }
+            if (stderrText == null) {
+                logger.info("No readable stderr file?");
+                stderrText = "";
+            }
+
+            String[] lines = stderrText.split("[\\r\\n]+");
+            int stop = -1;
+            int i = lines.length;
+            Pattern pattern = Pattern.compile("^### ERROR: COMPILER_ERROR: (?:[0-9]+) ###$");
+            while (i > 0) {
+                String line = lines[--i];
+                logger.trace("> " + line);
+                if (stop < 0) {
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.matches()) {
+                        stop = i;
+                    }
+                } else {
+                    if ("### COMPILE FILES ###".equals(line)) {
+                        String notes = "";
+                        for (int j = i + 1; j < stop; ++j) {
+                            notes += lines[j] + "\n";
+                        }
+                        output.setNotes(notes);
+                        return;
+                    }
+                }
+            }
+        } finally {
+            deleteFiles(files);
         }
     }
 
     private void updateOutputWhenModuleError(TestOutput output, String externalId) {
         List<String> files = getOutputFiles(externalId);
-        String stderrText = null;
-        for (String filePath : files) {
-            if (filePath.contains("stderr")) {
-                try {
-                    stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
-                    break;
-                } catch (IOException ex) {
-                    logger.error("Unable read stderr file", ex);
-                }
-            }
-        }
-        if (stderrText == null) {
-            logger.info("No readable stderr file?");
-            stderrText = "";
-        }
-
-        String[] lines = stderrText.split("[\\r\\n]+");
-        int stop = -1;
-        int i = lines.length;
-        Pattern pattern = Pattern.compile("^### ERROR: MODULE_ERROR: (?:[0-9]+) ###$");
-        while (i > 0) {
-            String line = lines[--i];
-            logger.trace("> " + line);
-            if (stop < 0) {
-                Matcher matcher = pattern.matcher(line);
-                if (matcher.matches()) {
-                    stop = i;
-                }
-            } else {
-                if ("### MODULE LOAD ###".equals(line)) {
-                    String notes = "";
-                    for (int j = i + 1; j < stop; ++j) {
-                        notes += lines[j] + "\n";
+        try {
+            String stderrText = null;
+            for (String filePath : files) {
+                if (filePath.contains("stderr")) {
+                    try {
+                        stderrText = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+                        break;
+                    } catch (IOException ex) {
+                        logger.error("Unable read stderr file", ex);
                     }
-                    output.setNotes(notes);
-                    return;
                 }
             }
+            if (stderrText == null) {
+                logger.info("No readable stderr file?");
+                stderrText = "";
+            }
+
+            String[] lines = stderrText.split("[\\r\\n]+");
+            int stop = -1;
+            int i = lines.length;
+            Pattern pattern = Pattern.compile("^### ERROR: MODULE_ERROR: (?:[0-9]+) ###$");
+            while (i > 0) {
+                String line = lines[--i];
+                logger.trace("> " + line);
+                if (stop < 0) {
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.matches()) {
+                        stop = i;
+                    }
+                } else {
+                    if ("### MODULE LOAD ###".equals(line)) {
+                        String notes = "";
+                        for (int j = i + 1; j < stop; ++j) {
+                            notes += lines[j] + "\n";
+                        }
+                        output.setNotes(notes);
+                        return;
+                    }
+                }
+            }
+        } finally {
+            deleteFiles(files);
         }
+    }
+
+    private void deleteFiles(List<String> filenames) {
+        if (filenames == null || filenames.isEmpty()) {
+            return;
+        }
+        logger.info("Deleting output files");
+        filenames.stream().map(name -> new File(name)).forEach(p -> {
+            logger.trace("deleting: " + p.toString());
+            p.delete();
+        });
     }
 
     private String executeCommand(List<String> command) {
