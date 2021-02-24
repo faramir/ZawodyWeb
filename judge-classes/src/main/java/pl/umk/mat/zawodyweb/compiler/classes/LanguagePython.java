ab34ad0e84c4c972d77c2fa465d18dd8a3aa4d07
Marek Nowicki <faramir2@gmail.com>
2021-02-24 11:14:29 +0100

    Information about error in python language execution goes to a log.

--- judge-classes/src/main/java/pl/umk/mat/zawodyweb/compiler/classes/LanguagePython.java
+++ judge-classes/src/main/java/pl/umk/mat/zawodyweb/compiler/classes/LanguagePython.java
@@ -7,19 +7,25 @@
  */
 package pl.umk.mat.zawodyweb.compiler.classes;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
-import pl.umk.mat.zawodyweb.judge.commons.TestInput;
-import pl.umk.mat.zawodyweb.judge.commons.TestOutput;
-import pl.umk.mat.zawodyweb.judge.commons.CompilerInterface;
 import pl.umk.mat.zawodyweb.database.ResultsStatusEnum;
+import pl.umk.mat.zawodyweb.judge.commons.CompilerInterface;
 import pl.umk.mat.zawodyweb.judge.commons.InterruptTimer;
 import pl.umk.mat.zawodyweb.judge.commons.ReaderEater;
+import pl.umk.mat.zawodyweb.judge.commons.TestInput;
+import pl.umk.mat.zawodyweb.judge.commons.TestOutput;
 import pl.umk.mat.zawodyweb.judge.commons.WriterFeeder;
 
 /**
@@ -28,6 +34,46 @@ import pl.umk.mat.zawodyweb.judge.commons.WriterFeeder;
 public class LanguagePython implements CompilerInterface {
 
     public static final org.apache.log4j.Logger logger = Logger.getLogger(LanguagePython.class);
+    public static final String FAKE_IMPORTER =
+            "# ---------------------------------------------------------------------------\n"
+                    + "class FakeImporter(object):\n"
+                    + "    def __init__(self):\n"
+                    + "        from imp import new_module\n"
+                    + "        self.new_module = new_module\n"
+                    + "        import sys\n"
+                    + "        self.sys = sys\n"
+                    + "        sys.meta_path = [self]\n"
+                    + "#        modules = [mod for mod in sys.modules.keys()\n"
+                    + "#                       if not mod.startswith('_')\n"
+                    + "#                      and not mod.startswith('encodings')\n"
+                    + "#                      and not mod.startswith('exceptions')\n"
+                    + "#                      and not mod.startswith('sre')]\n"
+                    + "        modules = [mod for mod in sys.modules.keys()\n"
+                    + "                       if mod.startswith('zipimport')\n"
+                    + "                      and mod.startswith('os')\n"
+                    + "                      and mod.startswith('atexit')\n"
+                    + "                      and mod.startswith('dis')\n"
+                    + "                      and mod.startswith('site')\n"
+                    + "                      and mod.startswith('opcode')]\n"
+                    + "        for mod in modules: del sys.modules[mod]\n"
+                    + "    def find_module(self, fullname, path=None): return self\n"
+                    + "    def load_module(self, fullname):\n"
+                    + "        mod = self.new_module(fullname)\n"
+                    + "        mod.__loader__ = self\n"
+                    + "        self.sys.modules[fullname] = mod\n"
+                    + "        mod.__file__ = '[fake module %r]' % fullname\n"
+                    + "        mod.__path__ = []\n"
+                    + "        return mod\n" // lub `throw ImportError`
+                    + "\n"
+                    + "FakeImporter()\n"
+                    + "\n"
+                    + "# __builtins__.open = None\n"
+                    + "del __builtins__.open\n"
+                    + "del __builtins__.execfile\n"
+                    + "del __builtins__.file\n"
+                    + "del __builtins__.compile\n"
+                    + "del __builtins__.reload\n"
+                    + "# ---------------------------------------------------------------------------\n";
     private Properties properties;
     private int compileResult = ResultsStatusEnum.UNDEF.getCode();
     private String compileDesc = "";
@@ -52,7 +98,7 @@ public class LanguagePython implements CompilerInterface {
     /**
      * Run user program (using path) on input generating output
      *
-     * @param path path to file to execute
+     * @param path  path to file to execute
      * @param input input values
      * @return output values
      */
@@ -105,29 +151,18 @@ public class LanguagePython implements CompilerInterface {
                 threadReaderEater.join();
                 threadWriterFeeder.join();
 
-//                if (readerEater.getException() != null) {
-//                    throw readerEater.getException();
-//                }
-
                 outputText = readerEater.getOutputText();
             } catch (InterruptedException ex) {
                 output.setRuntime(input.getTimeLimit());
                 output.setStatus(ResultsStatusEnum.TLE.getCode());
                 logger.debug("TLE after " + (System.currentTimeMillis() - time) + "ms.", ex);
                 return output;
-//            } catch (IOException ex) {
-//                logger.fatal("IOException", ex);
-//                p.destroy();
             } catch (Exception ex) {
                 logger.fatal("Fatal Exception", ex);
                 exception = true;
             } finally {
-                if (timer != null) {
-                    timer.cancel();
-                }
-                if (p != null) {
-                    p.destroy();
-                }
+                timer.cancel();
+                p.destroy();
                 if (threadReaderEater != null) {
                     threadReaderEater.interrupt();
                 }
@@ -152,6 +187,7 @@ public class LanguagePython implements CompilerInterface {
 
             try {
                 if (p.exitValue() != 0) {
+                    logger.debug("Abnormal Program termination.\nExit status: " + p.exitValue() + "\n" + outputText);
                     output.setStatus(ResultsStatusEnum.RE.getCode());
                     output.setNotes("Abnormal Program termination.\nExit status: " + p.exitValue() + "\n");
                     return output;
@@ -175,66 +211,29 @@ public class LanguagePython implements CompilerInterface {
      * <p>After first line comments inserts FakeImporter to
      * disable importing modules that are not standard and disabling open
      * builtin function</p>
+     *
      * @param bytes user solution source code
      * @return modified source code
      */
     @Override
     public byte[] precompile(byte[] bytes) {
+        String fullCode = new String(bytes);
         Pattern pattern = Pattern.compile("(^(?:#[^\n]*\n)*)([^#].*$)", Pattern.DOTALL);
-        Matcher matcher = pattern.matcher(new String(bytes));
-        if (matcher.matches() == false) {//|| matcher.groupCount() != 2
-            return bytes;
+        Matcher matcher = pattern.matcher(fullCode);
+        if (!matcher.matches()) {//|| matcher.groupCount() != 2
+            return (FAKE_IMPORTER + "\n" + fullCode).getBytes();
         }
 
         String header = matcher.group(1);
         String code = matcher.group(2);
 
-        return (header + "\n"
-                + "# ---------------------------------------------------------------------------\n"
-                + "class FakeImporter(object):\n"
-                + "    def __init__(self):\n"
-                + "        from imp import new_module\n"
-                + "        self.new_module = new_module\n"
-                + "        import sys\n"
-                + "        self.sys = sys\n"
-                + "        sys.meta_path = [self]\n"
-                + "#        modules = [mod for mod in sys.modules.keys()\n"
-                + "#                       if not mod.startswith('_')\n"
-                + "#                      and not mod.startswith('encodings')\n"
-                + "#                      and not mod.startswith('exceptions')\n"
-                + "#                      and not mod.startswith('sre')]\n"
-                + "        modules = [mod for mod in sys.modules.keys()\n"
-                + "                       if mod.startswith('zipimport')\n"
-                + "                      and mod.startswith('os')\n"
-                + "                      and mod.startswith('atexit')\n"
-                + "                      and mod.startswith('dis')\n"
-                + "                      and mod.startswith('site')\n"
-                + "                      and mod.startswith('opcode')]\n"
-                + "        for mod in modules: del sys.modules[mod]\n"
-                + "    def find_module(self, fullname, path=None): return self\n"
-                + "    def load_module(self, fullname):\n"
-                + "        mod = self.new_module(fullname)\n"
-                + "        mod.__loader__ = self\n"
-                + "        self.sys.modules[fullname] = mod\n"
-                + "        mod.__file__ = '[fake module %r]' % fullname\n"
-                + "        mod.__path__ = []\n"
-                + "        return mod\n" // lub `throw ImportError`
-                + "\n"
-                + "FakeImporter()\n"
-                + "\n"
-                + "# __builtins__.open = None\n"
-                + "del __builtins__.open\n"
-                + "del __builtins__.execfile\n"
-                + "del __builtins__.file\n"
-                + "del __builtins__.compile\n"
-                + "del __builtins__.reload\n"
-                + "# ---------------------------------------------------------------------------\n"
-                + "\n" + code).getBytes();
+        return (header + "\n" + FAKE_IMPORTER + "\n" + code).getBytes();
     }
 
     /**
      * Compiling code.
      * <p>Creating .py file</p>
+     *
      * @param bytes user solution code
      * @return generated executable file
      */
@@ -269,6 +268,7 @@ public class LanguagePython implements CompilerInterface {
 
     /**
      * Modifing path to execute after compiling
+     *
      * @param path compiled file path
      * @return modified compled file path
      */
@@ -279,6 +279,7 @@ public class LanguagePython implements CompilerInterface {
 
     /**
      * Clearing after executing all tests.
+     *
      * @param path compiled file path
      */
     @Override
